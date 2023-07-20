9d01f7398395a24e58edb61b5aa55d59f729a8db
luan.ferro <luan.ferro@sensedia.com>
2023-07-20 00:03:58 -0300

    commit initial

---
+++ src/test/java/com/luanferro/sistemajava/SistemajavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luanferro.sistemajava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemajavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
