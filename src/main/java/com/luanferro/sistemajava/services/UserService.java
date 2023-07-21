818e674b849ccad321178f345077ada63770c92a
luan.ferro <luan.ferro@sensedia.com>
2023-07-20 22:31:08 -0300

    exception handling - delete

--- src/main/java/com/luanferro/sistemajava/services/UserService.java
+++ src/main/java/com/luanferro/sistemajava/services/UserService.java
@@ -4,10 +4,14 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessResourceFailureException;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.luanferro.sistemajava.entities.User;
 import com.luanferro.sistemajava.repositories.UserRepository;
+import com.luanferro.sistemajava.services.exceptions.DatabaseException;
 import com.luanferro.sistemajava.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -30,7 +34,14 @@ public class UserService {
     }
 
     public void delete(Long id){
-       repository.deleteById(id); 
+        try {
+            repository.deleteById(id); 
+        } catch(EmptyResultDataAccessException e) {
+            throw new ResourceNotFoundException(id);
+        } catch(DataIntegrityViolationException e) {
+            throw new DatabaseException(e.getMessage());
+        }
+       
     }
 
     public User update(Long id, User obj) {
