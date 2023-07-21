eca430ce5cbe904f725ecbf0432054e8d69e378c
luan.ferro <luan.ferro@sensedia.com>
2023-07-20 22:24:44 -0300

    exception handling - findById

--- src/main/java/com/luanferro/sistemajava/services/UserService.java
+++ src/main/java/com/luanferro/sistemajava/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.luanferro.sistemajava.entities.User;
 import com.luanferro.sistemajava.repositories.UserRepository;
+import com.luanferro.sistemajava.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -21,7 +22,7 @@ public class UserService {
     
     public User findById(Long id) {
         Optional<User> obj = repository.findById(id);
-        return obj.get();
+        return obj.orElseThrow(() -> new ResourceNotFoundException(id));
     }
 
     public User insert(User obj) {
